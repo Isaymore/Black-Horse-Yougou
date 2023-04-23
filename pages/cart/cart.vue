904a1dd83a38eab2c1e2e46d52b183569237e04e
liyebin <13413697746@163.com>
2023-04-23 17:26:54 +0800

    分类开发

--- pages/cart/cart.vue
+++ pages/cart/cart.vue
@@ -1,22 +1,36 @@
 <template>
-	<view>
-		
-	</view>
+  <view class="cate-wrap">
+    <!-- 左侧的滚动视图区域 -->
+    <scroll-view class="cate-left" scroll-y="true">
+      <view class="cate-left-item active">电脑</view>
+      <view class="cate-left-item">电脑</view>
+      <view class="cate-left-item">电脑</view>
+      <view class="cate-left-item">电脑</view>
+      <view class="cate-left-item">电脑</view>
+    </scroll-view>
+    <!-- 右侧的滚动视图区域 -->
+    <scroll-view class="cate-right" scroll-y="true">
+      <view class="cate-left-item">电脑</view>
+      <view class="cate-left-item">电脑</view>
+      <view class="cate-left-item">电脑</view>
+      <view class="cate-left-item">电脑</view>
+    </scroll-view>
+  </view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				
-			}
-		},
-		methods: {
-			
-		}
-	}
+  export default {
+    data() {
+      return {
+
+      }
+    },
+    methods: {
+
+    }
+  }
 </script>
 
-<style>
-
+<style lang="scss">
+@import url('./style/cart.scss');
 </style>
--- pages/cate/cate.vue
+++ pages/cate/cate.vue
@@ -1,22 +1,46 @@
 <template>
-	<view>
-		
-	</view>
+  <view class="cate-wrap">
+    <!-- 左侧的滚动视图区域 -->
+    <scroll-view class="cate-scroll-left" scroll-y="true">
+      <view class="cate-scroll-left-item active">电脑</view>
+      <view class="cate-scroll-left-item">电脑</view>
+      <view class="cate-scroll-left-item">电脑</view>
+      <view class="cate-scroll-left-item">电脑</view>
+      <view class="cate-scroll-left-item">电脑</view>
+    </scroll-view>
+    <!-- 右侧的滚动视图区域 -->
+    <scroll-view class="cate-scroll-right" scroll-y="true">
+      <view class="cate-scroll-right-item">电脑</view>
+      <view class="cate-scroll-right-item">电脑</view>
+      <view class="cate-scroll-right-item">电脑</view>
+      <view class="cate-scroll-right-item">电脑</view>
+    </scroll-view>
+  </view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				
-			}
-		},
-		methods: {
-			
-		}
-	}
+  export default {
+    data() {
+      return {
+        // 动态计算窗口的剩余高度
+        // 窗口的可用高度 = 屏幕高度 - navigationBar高度 - tabBar 高度
+        windowHeight: 0,
+        active: 0
+      }
+    },
+    onReady() {
+      // 获取当前系统的信息
+      const systemInfo = uni.getSystemInfoSync()
+      console.log('星爷-onReady-systemInfo', systemInfo)
+      // 给窗口可用高度赋值
+      this.windowHeight = systemInfo.windowHeight
+    },
+    methods: {
+
+    }
+  }
 </script>
 
-<style>
-
+<style lang="less">
+  @import url('./style/cate.less');
 </style>
